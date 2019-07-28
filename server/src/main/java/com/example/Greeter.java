1d342eec2660d5ad3f08002a5a87e6cbddd210f0
piws <developer.ako@outlook.com>
2019-07-28 10:03:51 +0800

    Second commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,8 @@ public class Greeter {
 
   }
 
-  /**
-   * @param someone the name of a person
-   * @return greeting string
-   */
-  public final String greet(final String someone) {
+  // TO DO
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
